fdde62a243dcd95310fecea52049c3b2e8248990
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-20 15:37:34 -0400

    Setting view of mainlayout and added functionality to add button

--- app/src/main/java/com/example/contactapp/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.contactapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    //Creating Variables
+    private var fab: FloatingActionButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initializing floating action button view
+        fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        //send to AddorEditActivity when clicked on the floating action button
+        fab?.setOnClickListener {
+            val i = Intent(applicationContext, CreateModify::class.java)
+            startActivity(i)
+        }
     }
+
 }
