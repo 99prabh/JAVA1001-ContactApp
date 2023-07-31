4482c7282b471c51564c4c34b1ea32f2cee65a3e
99prabh <133125732+99prabh@users.noreply.github.com>
2023-07-31 11:06:59 -0400

    Added Model class

---
+++ app/src/main/java/com/example/contactapp/Model.kt
@@ -0,0 +1,10 @@
+package com.example.contactapp
+public class Model {
+    var id: Int = 0
+    var first_name: String = ""
+    var last_name: String = ""
+    var contact_number: String = ""
+    var email: String = ""
+    var address: String = ""
+    var byteArray: ByteArray = ByteArray(1000)
+}
