49bd2d40c466f39af71444842e566bd06f7fe4b8
99prabh <133125732+99prabh@users.noreply.github.com>
2023-08-07 11:17:21 -0400

    Added function to retrieve contact from DB

--- app/src/main/java/com/example/contactapp/Database.kt
+++ app/src/main/java/com/example/contactapp/Database.kt
@@ -21,6 +21,52 @@ class Database(context: Context) : SQLiteOpenHelper(context, Database.DB_NAME, n
         onCreate(db)
     }
 
+
+    //add new contact to the database by the contact model
+    fun addContact(tasks: Model): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(FIRSTNAME, tasks.first_name)
+        values.put(LASTNAME, tasks.last_name)
+        values.put(CONTACT, tasks.contact_number)
+        values.put(EMAIL, tasks.email)
+        values.put(ADDRESS, tasks.address)
+        values.put(PROFILE_IMAGE, tasks.byteArray)
+        val _success = db.insert(TABLE_NAME, null, values)
+        db.close()
+        Log.v("InsertedId", "$_success")
+        return (Integer.parseInt("$_success") != -1)
+    }
+
+
+    @SuppressLint("Range")
+    //getting all contacts from the database
+    fun contact(): List<Model> {
+        val contactList = ArrayList<Model>()
+        val db = writableDatabase
+        val selectQuery = "SELECT  * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(selectQuery, null)
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    val contact = Model()
+                    contact.id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ID)))
+                    contact.first_name = cursor.getString(cursor.getColumnIndex(FIRSTNAME))
+                    contact.last_name = cursor.getString(cursor.getColumnIndex(LASTNAME))
+                    contact.contact_number = cursor.getString(cursor.getColumnIndex(CONTACT))
+                    contact.email = cursor.getString(cursor.getColumnIndex(EMAIL))
+                    contact.address = cursor.getString(cursor.getColumnIndex(ADDRESS))
+                    contact.byteArray = cursor.getBlob(cursor.getColumnIndex(PROFILE_IMAGE))
+                    contactList.add(contact)
+                } while (cursor.moveToNext())
+            }
+        }
+        cursor.close()
+        return contactList
+    }
+
+
+
     //creating all the required variables
     companion object {
         private const val DB_VERSION = 1
